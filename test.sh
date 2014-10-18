a5a31cdff6b79b60e293d410eca8cf262a32656c
Milind S. Pandit <mspandit@yahoo.com>
2014-10-18 16:39:20 -0700

    Improved tests.

--- test.sh
+++ test.sh
@@ -2,7 +2,7 @@
 rm model*
 make clean
 CC=g++ make -e
-./rnnlm -train train -valid valid -rnnlm model
+./rnnlm -train train -valid valid -rnnlm model -hidden 15 -rand-seed 1 -debug 2 -class 100 -bptt 4 -bptt-block 10 -direct-order 3 -direct 2
 echo "**************************"
 echo "OK if no further output..."
 echo "**************************"
